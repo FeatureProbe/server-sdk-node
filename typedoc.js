de4ec476ea001bdc0430a5f5be48cec5ab66de96
HeZean <realhezean@gmail.com>
2022-11-03 17:10:03 +0800

    docs: change dir

--- typedoc.js
+++ typedoc.js
@@ -2,7 +2,7 @@ const pkg = require('./package.json');
 const PKG_VERSION = pkg.version;
 
 module.exports = {
-  out: './doc',
+  out: './docs',
   name: `FeatureProbe Server Side SDK for NodeJS (${PKG_VERSION})`,
   readme: 'README.md',
   entryPoints: ['./src/index.ts']
